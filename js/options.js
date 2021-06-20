e5abf39fdb78cda66775ab897034e998ed4603bd
SCVODArchives <djpnuemo@yahoo.com>
2021-06-19 17:10:56 -0700

    first commit

---
+++ js/options.js
@@ -0,0 +1,32 @@
+let options = {};
+let optionEls = document.querySelectorAll("input");
+
+chrome.storage.sync.get("options", (data) => {
+    console.log(data.options);
+    if (data.options){
+        for (o in data.options){
+            document.querySelector(`#${o}`).checked = Boolean(data.options[o]);
+        }
+        Object.assign(options, data.options);
+    }else{
+    }
+});
+
+for (let o = 0; o < optionEls.length; o++){
+    optionEls[o].addEventListener("change", (e) => {
+        options[optionEls[o].id] = Boolean(optionEls[o].checked);
+        chrome.storage.sync.set({ options });
+        console.log(options);
+    });
+}
+
+/*document.querySelector("#playerInfoDetailed").addEventListener("change", (e) => {
+    if (e.target.checked){
+        var info = document.getElementById("playerInfo");
+        info.checked = true;
+        info.disabled = true;
+        info.dispatchEvent(new Event("change"));
+    }else{
+        document.querySelector("#playerInfo").disabled = false;
+    }
+});*/
